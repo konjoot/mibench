04a5b29d0489ffd56940d517402dfb34c2d69476
konjoot <konjoot@gmail.com>
2015-04-26 18:31:15 +0300

    add activesupport and sshkit gems + some progress in functionality

--- bench.rb
+++ bench.rb
@@ -1,3 +1,6 @@
+require 'active_support/core_ext/string/inflections'
+require 'sshkit/dsl'
+
 module Bench
 
   def for (app)
@@ -8,12 +11,13 @@ module Bench
   module_function :for
 
   class App
-    attr_reader :app, :name
+    attr_reader :app, :name, :f_name
 
-    APPS = %w{pliny, gin}.freeze
+    APPS = %i{pliny gin}
 
     def initialize(name)
       @name = name
+      @f_name = "Bench::#{name.to_s.camelize}"
       @app  = build_app
     end
 
@@ -23,12 +27,14 @@ module Bench
 
     def build_app
       raise 'unknown app name' if invalid?
-      app_cls = name.to_s.camelize.constantize
+      p f_name
+      app_cls = f_name.constantize
       return app_cls.new if app_cls.respond_to? :new
       raise "can't initialize #{name}"
     end
 
     def valid?
+      p APPS
       APPS.include?(name)
     end
 
@@ -44,12 +50,19 @@ module Bench
     NAME = :pliny
 
     def initialize
-      @config = Config.new(NAME)
+      @config = Bench::Config.new(NAME)
     end
 
     def start
-      return "#{NAME} allready running!" if running?
-      %x{#{cname} #{args}}
+      on :localhost do
+        within '/home/konjoot/projects/mecroservice_experiments/pliny' do
+          with rack_env: :production, daemonize: :true do
+            execute :foreman, :start
+          end
+        end
+      end
+      # return "#{NAME} allready running!" if running?
+      # %x{#{cname} #{args}}
     end
 
     def running?
@@ -60,6 +73,9 @@ module Bench
 
     def args
     end
+  end
+
+  class Config
   end
 
   class Gin

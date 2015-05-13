f94b92ffe40e6dfb22361345d6fe81f34e9e260f
konjoot <konjoot@gmail.com>
2015-05-14 00:51:01 +0300

    working on new dsl -> stop command for pliny ended

--- bench.rb
+++ bench.rb
@@ -1,61 +1,50 @@
 require 'sshkit'
 require 'sshkit/dsl'
 require 'active_support/core_ext/string/inflections'
+require 'active_support/core_ext/module/delegation'
 SSHKit.config.output_verbosity = :debug
 
 module Bench
-  attr_reader :name, :app, :bencher
 
   def mark(&block)
     self.instance_eval &block
   end
 
   def with(name, &block)
-    puts 'test'
-  end
+    app  = App.new(name)
 
+    app.start && sleep(1)
+
+    p block
+
+    sleep(1) && app.stop
+  end
 
   module_function :mark, :with
 
-  private
-
-  def app
-    @app ||= App.new(name)
-  end
-
-  module_function :app
-
-  def bencher
-    @bencher ||= Bencher.new()
-  end
-
-  module_function :bencher
-
   class App
     attr_reader :app, :name, :f_name
+    delegate :stop, :start, to: :app
 
     APPS = %i{pliny gin}.freeze
 
     def initialize(name)
       @name = name
       @f_name = "Bench::#{name.to_s.camelize}"
-      @app  = build_app
+      @app  = build
     end
 
-    def start
-      app.start
-    end
-
-    def build_app
+    def build
       raise 'unknown app name' if invalid?
-      p f_name
+
       app_cls = f_name.constantize
+
       return app_cls.new if app_cls.respond_to? :new
+
       raise "can't initialize #{name}"
     end
 
     def valid?
-      p APPS
       APPS.include?(name)
     end
 
@@ -69,7 +58,7 @@ module Bench
 
     attr_reader :config
 
-    NAME = :pliny
+    NAME = 'pliny'
 
     def initialize
       @config = Bench::Config.new(NAME)
@@ -79,12 +68,20 @@ module Bench
       run_locally do
         within '/home/konjoot/projects/microservice_experiments/pliny' do
           with rack_env: :production, daemonize: :true do
-            execute :rvm, '2.2.1@pliny', :do, :foreman, :start
+            execute :rvm, :'2.2.1@pliny', :do, :foreman, :start
           end
         end
       end
       # return "#{NAME} allready running!" if running?
       # %x{#{cname} #{args}}
+    end
+
+    def stop
+      run_locally do
+        within '/home/konjoot/projects/microservice_experiments/pliny' do
+          execute :rvm, :'2.2.1@pliny do pumactl -P .pid stop'
+        end
+      end
     end
 
     def running?
